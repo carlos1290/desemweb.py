19b42e256464ab5e76c1a6ae99953c8c1b22bb8e
ceibal <you@example.com>
2024-04-17 19:51:25 -0300

    second commit

--- menudesplegable.js
+++ menudesplegable.js
@@ -1,12 +1,12 @@
-// Obtener el elemento del menú desplegable
+
 var menuProgramacion = document.getElementById("menuProgramacion");
 
-// Agregar evento mouseover para mostrar el menú desplegable
+
 menuProgramacion.addEventListener("mouseover", function () {
   this.getElementsByClassName("dropdown-menu")[0].classList.add("show");
 });
 
-// Agregar evento mouseout para ocultar el menú desplegable
+
 menuProgramacion.addEventListener("mouseout", function () {
   this.getElementsByClassName("dropdown-menu")[0].classList.remove("show");
 });
